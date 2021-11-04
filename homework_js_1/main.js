9119c3c7fc0217f160c25d35a73bdf6e453e40dd
sergeysidyuk <44618749+sergeysidyuk@users.noreply.github.com>
2021-11-04 15:23:48 +0200

    Create main.js

---
+++ homework_js_1/main.js
@@ -0,0 +1,13 @@
+let USER = 'Sergey';
+let AGE = 22;
+let GENDER = 'Male';
+let HEIGHT = 1.80;
+let WEIGHT = 70;
+let EYE_COLOR = 'green';
+
+console.log(USER , typeof(USER));
+console.log(AGE , typeof(AGE));
+console.log(GENDER, typeof(GENDER));
+console.log(HEIGHT, typeof(HEIGHT));
+console.log(WEIGHT, typeof(WEIGHT));
+console.log(EYE_COLOR, typeof(EYE_COLOR));
