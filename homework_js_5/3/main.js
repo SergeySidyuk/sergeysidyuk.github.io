0148f7dbeb24ac00f6ee539309a02418328f16db
sergeysidyuk <44618749+sergeysidyuk@users.noreply.github.com>
2021-11-05 22:28:32 +0200

    Update main.js

--- homework_js_5/3/main.js
+++ homework_js_5/3/main.js
@@ -1,16 +1,19 @@
-// 3. Задача на синтаксис объектов. Напишите код, по строке на каждое действие.
-// •  Создайте пустой объект user.
-// •  Добавьте свойство name со значением Вася.
-// •  Добавьте свойство surname со значением Петров.
-// •  Поменяйте значение name на Сергей.
-// •  Удалите свойство name из объекта.
+// 3. Напишите функцию get_string_length(str), 
+// которая в качестве аргумента принимает строку str и возвращает её длину, 
+// предварительно удалив из строки все пробелы 
+// (для решения данной задачи обязательно использовать цикл).
 
-let user ={}
- user['name'] = 'Вася'
- console.log(user)
- user['surname'] = 'Петров'
- console.log(user)
- user['name'] = 'Сергей'
- console.log(user)
- delete user.name
-console.log(user)
+function get_string_length(str) {
+    let result = str;
+
+    if (str) {
+        let temp = str.split(' ').join('')
+        result = `${temp}`;
+    }
+    return result ;
+}
+let a = get_string_length('Что было бы если бы не было пробелов');
+console.log(`${a} , Количество символов :${a.length}`);
+
+
+
